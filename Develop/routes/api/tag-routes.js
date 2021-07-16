de18b75557466e6e761758c8f8b61d5062fb97f3
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-15 23:15:51 -0500

    added Tag.findAll function and its associated product data into tag-routes.js file

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -4,25 +4,39 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+    // find all tags
+    Tag.findAll({
+            attributes: ["id", "tag_name"],
+            include: [{
+                model: Product,
+                attributes: ["id", "product_name", "price", "stock", "category_id"],
+                through: ProductTag,
+                as: "products",
+            }, ],
+        })
+        // be sure to include its associated Product data
+        .then((dbTagData) => res.json(dbTagData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+    // create a new tag
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+    // delete on tag by its `id` value
 });
 
 module.exports = router;
