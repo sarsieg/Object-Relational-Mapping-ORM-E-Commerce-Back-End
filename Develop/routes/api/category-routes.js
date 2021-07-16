edcebd8b7365c6e0db54fb2750cbf755cedb046e
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-15 22:23:58 -0500

    added find one category by its 'id' value and included its associated products

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -22,7 +22,29 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     // find one category by its `id` value
-    // be sure to include its associated Products
+    Category.findOne({
+            where: {
+                id: req.params.id,
+            },
+            include: [{
+                model: Product,
+                attributes: ["id", "product_name", "price", "stock", "category_id"],
+            }, ],
+        })
+        // be sure to include its associated Products
+        .then((dbCategoryData) => {
+            if (!dbCategoryData) {
+                res
+                    .status(404)
+                    .json({ message: "There was no category found for this id." });
+                return;
+            }
+            res.json(dbCategoryData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
