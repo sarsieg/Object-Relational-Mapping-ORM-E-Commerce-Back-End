ab121e84f7b4b7973d6c6fdbc695329f97609c3a
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-15 22:19:47 -0500

    added find all categories and included its associated products within the category-routes.js file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,25 +4,37 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // find all categories
+    Category.findAll({
+            attributes: ["id", "category_name"],
+            include: [{
+                model: Product,
+                attributes: ["id", "product_name", "price", "stock", "category_id"],
+            }, ],
+        })
+        // be sure to include its associated Products
+        .then((dbCategoryData) => res.json(dbCategoryData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    // find one category by its `id` value
+    // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+    // create a new category
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
 });
 
 module.exports = router;
